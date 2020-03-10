d32c8547f15138e1f1d6d3e1a7b5bb2b16400f86
Swapna <swapnasairam2007@gmail.com>
2020-03-10 19:20:56 +0000

    Sending java data types to git

---
+++ src/learn/programming/MyPrimitiveTest.java
@@ -0,0 +1,27 @@
+package learn.programming;
+
+public class MyPrimitiveTest {
+    static byte myByteExample;
+    static short myShortExample;
+    static int myIntExample;
+    static long myLongExample;
+    static float myfloatExample;
+    static double mydoubleExample;
+    static char myCharExample;
+    static boolean myBooleanExample;
+
+    public static void main(String[] args) {
+
+
+        System.out.println(myByteExample);
+        System.out.println(myShortExample);
+        System.out.println(myIntExample);
+        System.out.println(myLongExample);
+        System.out.println(myfloatExample);
+        System.out.println(mydoubleExample);
+        System.out.println(myCharExample);
+        System.out.println(myBooleanExample);
+
+    }
+
+}
